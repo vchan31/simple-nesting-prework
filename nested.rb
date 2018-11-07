2bad7deba18598541593b98e9a654a48a0dfbb4d
sensible-proc-7557 <vchan313@gmail.com>
2018-11-07 20:34:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for]     
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+programmer_hash[:yukihiro_matsumoto] => {:known_for => 'Ruby', :languages => ['LISP, C']
+  
+}
     
 end
 
